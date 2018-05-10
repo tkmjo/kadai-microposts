842efe04f71976dff789e518ea9bb11fdfe1e89b
tkmjo <t.kamijo87395426@gmail.com>
2018-05-10 15:19:55 +0000

    user pages

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -211,6 +211,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Gravatar' => Tomaswelton\LaravelGravatar\Facades\Gravatar::class,
     ],
 
 ];
