7169c04c568e5874801efa2760651c4d4e33cef6
tkmjo <t.kamijo87395426@gmail.com>
2018-05-15 11:54:08 +0000

    Procfile modified

--- app/User.php
+++ app/User.php
@@ -31,4 +31,47 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
+    
+    public function followings() 
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+    
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+    public function follow($userId)
+    {
+        // すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 自分自身ではないかの確認
+        $its_me = $this->id == $userId;
+        
+        if ($exist || $its_me) {
+            return false;
+        } else {
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    
+    public function unfollow($user_id)
+    {
+        // すでにフォローしているかの確認
+        $exist = $this->is_following($user_id);
+        $its_me = $this->id == $user_id;
+        
+        if ($exist && !$its_me) {
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function is_following($userId) {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
 }
