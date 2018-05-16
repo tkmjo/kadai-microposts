5633dbd6ff429d3a58423a927db6d347205390c7
tkmjo <t.kamijo87395426@gmail.com>
2018-05-16 04:01:24 +0000

    timeline

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -22,4 +22,18 @@ class Controller extends BaseController
             'count_followers' => $count_followers,
         ];
     }
+    
+    public function index()
+    {
+        $data = [];
+        if (\Auth::ckeck()) {
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+            ];
+        }
+        return view('welcome', $data);
+    }
 }
